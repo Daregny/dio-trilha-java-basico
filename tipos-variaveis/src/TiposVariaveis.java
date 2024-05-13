8438ce8c748b2ee7c9f1f2dbce27fbda253cbf64
Daregny <daregny@gmail.com>
2024-05-13 08:29:46 -0300

    Tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        String meuNome = "Yenny Odalis";
+
+        int salarioMinimo = 2500;
+
+        double outroSalario = 2.5;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;        
+        System.out.println("primeiro: " + numero);
+
+        numero = 10;
+        System.out.println("segundo: " + numero);
+
+        final double VALOR_DO_PI = 3.14;
     }
 }
